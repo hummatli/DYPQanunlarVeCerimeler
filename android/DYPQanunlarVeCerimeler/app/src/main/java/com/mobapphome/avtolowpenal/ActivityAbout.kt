52841085cb34eed90ab6acae8f471fb6b4c140f3
Sattar Hummatli <settarxan@gmail.com>
2017-07-03 16:43:49 +0400

    Added Kotlin support

---
+++ android/DYPQanunlarVeCerimeler/app/src/main/java/com/mobapphome/avtolowpenal/ActivityAbout.kt
@@ -0,0 +1,106 @@
+package com.mobapphome.avtolowpenal
+
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.mobapphome.avtolowpenal.other.Constants
+import com.mobapphome.mahandroidupdater.tools.MAHUpdaterController
+
+import android.support.v7.app.ActionBarActivity
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+
+class ActivityAbout : ActionBarActivity() {
+
+    internal var adView: AdView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        if (toolbar != null) {
+            setSupportActionBar(toolbar)
+            //toolbar.setNavigationIcon(R.drawable.ic_drawer);
+            //toolbar.setTitle("Title");
+            //toolbar.setLogo(R.drawable.ic_launcher);
+            CustomStatusBarDecorator.decorateStatusBar(this)
+        }
+
+
+        //For enabling up button -----------------------------------------------------------------
+        val actionBar = supportActionBar
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+        //actionBar.setIcon(R.drawable.ic_action_bar_icon);
+
+        //For AdMob--------------------------------------------------------------------------------
+
+        Log.i("Test", "Ad Avialbe")
+        // Create an ad.
+        adView = AdView(this)
+        adView!!.adSize = AdSize.SMART_BANNER
+        adView!!.adUnitId = getString(R.string.admob_banner_unit_id)
+
+        val layout = findViewById(R.id.admobAA) as LinearLayout
+        layout.visibility = View.GONE
+        layout.addView(adView)
+
+        val adRequest = AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("D4B5D733140370B86154E86D1D54E7F7") //galaxy s4
+                .addTestDevice("154222674FFA1A7378D5E13BD636F597")//768 Genimotion Emuliator
+                .addTestDevice("D76C969E1CDC600B5CDDC34A67F7E148")//600 Genimotion emuliator
+                .addTestDevice("7D74307340040DDA1D9A190FFCD6D510")//480 Genimotion
+                .addTestDevice("ACD7FEE7E5F36FFCDCCCF3852177E4D4")//768 Genimotion
+                .build()
+
+        // Start loading the ad in the background.
+        adView!!.loadAd(adRequest)
+        adView!!.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                // TODO Auto-generated method stub
+                super.onAdLoaded()
+                Log.i("Test", "Ad loaded")
+                layout.visibility = View.VISIBLE
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.about, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+        when (id) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (adView != null) {
+            adView!!.destroy()
+        }
+
+    }
+}
